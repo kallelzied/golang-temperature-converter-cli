fabf60b472b4ead982dfde94e58e529fda597ca1
kallelzied <kallelzied@live.com>
2022-01-10 10:09:50 +0100

    Reading keyboards inputs

--- main.go
+++ main.go
@@ -24,11 +24,27 @@ func main() {
 	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput);
+			continue
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
-
-		if shouldConvertAgain != "Y" {
+		fmt.Scanln(&shouldConvertAgain)
+		
+		if err != nil {
+			printError(errReadingInput);
+			continue
+		}
+		
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
